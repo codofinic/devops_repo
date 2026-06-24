5fa5c5868a335e6f00b91e26f78d348e06adb8d8
Bhargav <bhargavpoorni@gmail.com>
2026-06-24 10:16:14 +0530

    first commit

---
+++ Script.js
@@ -0,0 +1,8 @@
+
+//  const api = async ()=>{
+//   const resp=await fetch('https://fakestoreapi.com/products/2')
+//   const data=await resp.json()
+//   console.log(data)
+//  }
+//  api()
+console.log("node js")
